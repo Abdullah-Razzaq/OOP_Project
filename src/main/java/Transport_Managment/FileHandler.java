3ba2862cf73acba098ccf53d3e91dc69880e2e57
Abdullah-Razzaq <abdullahrazzaq004@gmail.com>
2023-11-23 01:19:21 +0500

    main

--- src/main/java/Transport_Managment/FileHandler.java
+++ src/main/java/Transport_Managment/FileHandler.java
@@ -37,7 +37,7 @@ public class FileHandler {
                 lines.add(line);
             }
         } catch (IOException e) {
-            System.err.println("Error reading from file: " + e.getMessage());
+            System.out.println(e);
         }
 
         return lines;
